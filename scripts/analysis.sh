ee7c0d87e0bf198408aea7573f77820017d3f2f2
TurtleRuss <tttturtleruss@gmail.com>
2024-10-18 19:18:18 +0800

    update analysis.sh

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -21,6 +21,7 @@ link_bc_files() {
 
 TOP_DIR="$(pwd)/.."
 KERNEL_DIR="$TOP_DIR/linux"
+SCRIPT_DIR="$TOP_DIR/scripts"
 
 pushd "$KERNEL_DIR" > /dev/null || exit 1
 # first let's link all single bcfiles into one
@@ -29,6 +30,13 @@ link_bc_files ./drivers
 find . -name "combined.bc" | while read -r bcfile; do
     dir=$(dirname "$bcfile")
     name=$(dirname "$bcfile" | awk -F/ '{print $NF}')
-    echo "Running on $dir"
     wpa -ander -race -stat=false -dump-mssa -ind-call-limit=100000 -svfg "$bcfile" > "$dir/mssa.$name"
 done;
+
+find . -name "mssa.*" | while read -r mssa; do
+    dir=$(dirname "$mssa")
+    name=$(dirname "$mssa" | awk -F/ '{print $NF}')
+    python3 "$SCRIPT_DIR/gen_mempair.py" "$mssa" > "$dir/mempairs.$name"
+done;
+
+popd > /dev/null || exit 1
