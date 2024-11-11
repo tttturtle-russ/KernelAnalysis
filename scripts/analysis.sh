dacf20b341c912d5dac347baac7936d3ced0dbd0
TurtleRuss <tttturtleruss@gmail.com>
2024-11-12 01:34:05 +0800

    modify setup script and analyze script to adjust wllvm

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
 
-# Function to recursively find all directories and link .bc files
-link_bc_files() {
-    local dir="$1"
-    
-    # Find all .bc files in the current directory
-    bc_files=$(find "$dir" -maxdepth 1 -name "*.bc")
-    # If .bc files are found, link them together into combined.bc
-    if [ -n "$bc_files" ]; then
-        llvm-link $bc_files -o "$dir/combined.bc"
-    fi
-
-    # Recursively process subdirectories
-    for subdir in "$dir"/*/; do
-        if [ -d "$subdir" ]; then
-            link_bc_files "$subdir"
-        fi
-    done
-}
-
 TOP_DIR="$(pwd)/.."
 KERNEL_DIR="$TOP_DIR/linux"
 SCRIPT_DIR="$TOP_DIR/scripts"
 
 pushd "$KERNEL_DIR" > /dev/null || exit 1
-# first let's link all single bcfiles into one
-link_bc_files ./drivers
 
-find . -name "combined.bc" | while read -r bcfile; do
+find . -name "built-in.bc" | while read -r bcfile; do
     dir=$(dirname "$bcfile")
     name=$(dirname "$bcfile" | awk -F/ '{print $NF}')
     wpa -ander -cxt -race -stat=false -dump-mssa -ind-call-limit=100000 -svfg "$bcfile" > "$dir/mssa.$name"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,6 @@ TOP_DIR="$(pwd)/.."
 KERNEL_DIR="$TOP_DIR/linux"
 SVF_DIR="$TOP_DIR/SVF"
 LOG_DIR="$TOP_DIR/logs"
-CONFIG_DIR="$TOP_DIR/configs"
 
 # create log dir
 if [ ! -d "$LOG_DIR" ]; then
@@ -18,24 +17,33 @@ pushd "$SVF_DIR" > /dev/null || exit 1
 source ./build.sh > "$LOG_DIR/SVF.log"
 popd > /dev/null || exit 1
 
+# setup virtualenv
+echo "Setting up virtualenv"
+python3 -m venv venv
+source venv/bin/activate
+
+# install wllvm
+echo "Installing wllvm"
+pip3 install wllvm
+export LLVM_COMPILER=clang
+
 echo "Building Linux, see logs in $LOG_DIR/kernel_build.log"
 echo "For now only support linux-6.12-rc3"
 # build linux kernel bitcode files
 pushd "$KERNEL_DIR" > /dev/null || exit 1
-cp "$CONFIG_DIR/.config" .
-cp "$CONFIG_DIR/Makefile" .
-make CC=clang \
-    LD=ld.lld \
-    AR=llvm-ar \
-    NM=llvm-nm \
-    STRIP=llvm-strip \
-    OBJCOPY=llvm-objcopy \
-    OBJDUMP=llvm-objdump \
-    READELF=llvm-readelf \
-    HOSTCC=clang \
-    HOSTCXX=clang++ \
-    HOSTAR=llvm-ar \
-    HOSTLD=ld.lld \
-    V=0 \
-    -j"$(nproc)" > "$LOG_DIR/kernel_build.log"
+make CC=wllvm HOSTCC=wllvm allyesconfig
+make CC=wllvm HOSTCC=wllvm -j"$(nproc)" drivers/ > "$LOG_DIR/kernel_build.log"
+for dir in drivers/*; do
+        if [ -d "$dir" ]; then
+                if [ ! -f "$dir/built-in.a" ]; then
+                        continue
+                else
+                        find "$dir" -name built-in.a | while read -r archive; do
+                                extract-bc -b "$archive"
+                                dir_name=$(dirname "$archive")
+                                mv "$archive".bc "$dir_name"/built-in.bc
+                        done
+                fi
+        fi
+done
 popd > /dev/null || exit 1
