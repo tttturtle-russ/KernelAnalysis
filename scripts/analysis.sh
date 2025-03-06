2807dbc0a19df0f114693eb5bf72503087e334fe
TurtleRuss <tttturtleruss@gmail.com>
2025-03-06 19:38:31 +0800

    chore: change usage of the script

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -9,6 +9,10 @@ pushd "$KERNEL_DIR" >/dev/null || exit 1
 find . -mindepth 3 -maxdepth 3 -name "built-in.bc" | while read -r bcfile; do
   dir=$(dirname "$bcfile")
   name=$(dirname "$bcfile" | awk -F/ '{print $NF}')
+  if [ -f "$dir/mssa.$name" ]; then
+    echo "Skipping $name"
+    continue
+  fi
   echo "Analyzing $name"
   wpa -fspta -cxt -race -stat=false -dump-mssa -ind-call-limit=100000 "$bcfile" >"$dir/mssa.$name"
 done
@@ -17,7 +21,7 @@ find . -mindepth 3 -maxdepth 3 -name "mssa.*" | while read -r mssa; do
   dir=$(dirname "$mssa")
   name=$(dirname "$mssa" | awk -F/ '{print $NF}')
   echo "Generating $name"
-  python3 "$SCRIPT_DIR/gen_mempair.py" "$mssa" >"$dir/mempairs.$name"
+  python3 "$SCRIPT_DIR/gen_mempair.py" "$mssa" "$dir/mempairs.$name" "$dir/mapping.$name"
 done
 
 find . -mindepth 3 -maxdepth 3 -name "mempairs.*" -exec sh -c "sort {} | uniq" \; >mempairs_all
